fa3b3e3a9c25d81321af7b72d142421de45ad321
uopsdod <uopsdod@gmail.com>
2018-09-15 08:31:05 +0800

    refactoring code

--- Gitlab_Search/src/main/java/com/amazonaws/lambda/demo/search/SearchService.java
+++ Gitlab_Search/src/main/java/com/amazonaws/lambda/demo/search/SearchService.java
@@ -28,132 +28,98 @@ import j2html.tags.ContainerTag;
 import static j2html.TagCreator.*;
 
 public class SearchService {
+	private static final String NO_RESULT_FOUND = "no result found";
+	private static final String SERVER_ERROR = "server errors";
+	private static final String CLIENT_ERROR = "client errors";
 	
 	private org.apache.http.client.HttpClient client;
 	
 	public SearchService() {
-//		this.client = HttpClientBuilder.create().build();
-//		this.client = HttpClientBuilder.create().setRedirectStrategy(new DefaultRedirectStrategy()).build();
-//		this.client = HttpClientBuilder.create().setRedirectStrategy(new LaxRedirectStrategy()).build(); // not working ... 
 		this.client = HttpClients.custom()
-		        .setDefaultRequestConfig(RequestConfig.custom()
-		        .setCookieSpec(CookieSpecs.STANDARD).build())
-		        .build();
-		
-		
-		
-//		SchemeRegistry schemeRegistry = new SchemeRegistry();
-//		schemeRegistry.register(new Scheme("https", SSLSocketFactory.getSocketFactory(), 443));
-//		SingleClientConnManager mgr = new SingleClientConnManager(schemeRegistry);
-//		client = new DefaultHttpClient(mgr);
-		
-//		CloseableHttpClient httpclient = null;
-//		HttpHost target = new HttpHost('www.mysite.com', 443, "https");
-
-//		SSLContext sslcontext = SSLContexts.createSystemDefault();
-//		SSLConnectionSocketFactory sslConnectionSocketFactory = new SSLConnectionSocketFactory(
-//		        sslcontext, new String[] { "TLSv1", "SSLv3" }, null,
-//		        SSLConnectionSocketFactory.getDefaultHostnameVerifier());
-//
-//		Registry<ConnectionSocketFactory> socketFactoryRegistry = RegistryBuilder.<ConnectionSocketFactory>create()
-//		        .register("http", PlainConnectionSocketFactory.INSTANCE)
-//		        .register("https", sslConnectionSocketFactory)
-//		        .build();
-//		PoolingHttpClientConnectionManager cm = new PoolingHttpClientConnectionManager(socketFactoryRegistry);
-//
-//		httpclient = HttpClients.custom()
-//					.setSSLSocketFactory(sslConnectionSocketFactory)
-//					.setConnectionManager(cm)
-//					.build();
-//		
-//		this.client = httpclient;
+				        .setDefaultRequestConfig(RequestConfig.custom()
+				        						.setCookieSpec(CookieSpecs.STANDARD).build()) // we need to set this to prevent invalid cookie error 
+				        .build();
 	}
 	
 	public String search(String keyword) {
 		SearchUtil.getLogger().log(Level.INFO, "search starts");
 		List<String> searchObjs = new ArrayList<>();
-		StringBuilder result = new StringBuilder();
-		result.append("<html><head><title>HTML from API Gateway/Lambda</title></head>");
-		result.append("<body>");
-		result.append("<ul>");
 		try {
 			/** get authenticity_token from SignIn Page **/
 			HttpResponse getSignInPageHttpPostResp = this.client.execute(new GetSignInPageHttpGet());
 			String signInPageRespContent = EntityUtils.toString(getSignInPageHttpPostResp.getEntity());
-			SearchUtil.getLogger().log(Level.INFO, "here");
+			SearchUtil.getLogger().log(Level.INFO, "get sign-in page");
 			
 			String authenticity_token = "";
 			Pattern pattern = Pattern.compile("<input type=\"hidden\" name=\"authenticity_token\" value=\"(.*?)\" />");
 			Matcher matcher = pattern.matcher(signInPageRespContent);
 
-			if (matcher.find()) {
+			if (!matcher.find()) {
+				SearchUtil.getLogger().log(Level.INFO, "cannot find authentiction_token");
+				return SearchService.SERVER_ERROR + ": cannot find authentiction_token";
+			}else {
 				authenticity_token = matcher.group(1);
-				SearchUtil.getLogger().log(Level.INFO, "authenticity_token: " + authenticity_token);
+				SearchUtil.getLogger().log(Level.INFO, "get authenticity_token from sign-in page: " + authenticity_token);
 			}
 			
 			/** Sing in **/
 			HttpResponse loginHttpPostResp = this.client.execute(new LoginHttpPost("uopsdod@gmail.com", "eg359157", authenticity_token));
 			String loginRespContent = EntityUtils.toString(loginHttpPostResp.getEntity());
-			SearchUtil.getLogger().log(Level.INFO, "here");
+			if (loginRespContent.contains("Invalid Login or password")) {
+				SearchUtil.getLogger().log(Level.INFO, "Invalid Login or password");
+				return SearchService.CLIENT_ERROR + ": Invalid Login or password";
+			}else {
+				SearchUtil.getLogger().log(Level.INFO, "login successfully");
+			}
 			
-			/** Get Project List **/
+			/** Get Project Uri from Project List **/
 			HttpResponse getAllProjectsResp = this.client.execute(new GetAllProjectsHttpGet());
 			String getAllProjectsRespContent = EntityUtils.toString(getAllProjectsResp.getEntity());
-			SearchUtil.getLogger().log(Level.INFO, "here");
+			SearchUtil.getLogger().log(Level.INFO, "get all projects");
 			
-			// TODO: iterate the project list
-//			<a class="project" href="/uopsdod/test_search">
-			Pattern pattern_project = Pattern.compile("<a class=\"project\" href=\"(.*?)\">");
-			Matcher matcher_project = pattern_project.matcher(getAllProjectsRespContent);
-
+			Pattern pattern_getAllProjects = Pattern.compile("<a class=\"project\" href=\"(.*?)\">");
+			Matcher matcher_getAllProjects = pattern_getAllProjects.matcher(getAllProjectsRespContent);
 			
-			while (matcher_project.find()) {
-				String projct_uri = matcher_project.group(1);
-				SearchUtil.getLogger().log(Level.INFO, "projct_uri: " + projct_uri);
-				// TODO: get the project_id of each project
+			while (matcher_getAllProjects.find()) {
+				String projct_uri = matcher_getAllProjects.group(1);
+				SearchUtil.getLogger().log(Level.INFO, "get projct_uri: " + projct_uri);
+				
+				/** get the project_id of each project **/
 				HttpResponse getProjectResp = this.client.execute(new GetProjectHttpGet(projct_uri));
 				String getProjectRespContent = EntityUtils.toString(getProjectResp.getEntity());
-				SearchUtil.getLogger().log(Level.INFO, "here");
 				
-				// <input type="hidden" name="project_id" id="project_id" value="8382931" />
-				String projectId = "";
 				Pattern pattern_projectId = Pattern.compile("<input type=\"hidden\" name=\"project_id\" id=\"project_id\" value=\"(.*?)\" />");
 				Matcher matcher_projectId = pattern_projectId.matcher(getProjectRespContent);
 
 				if (matcher_projectId.find()) {
-					projectId = matcher_projectId.group(1);
-					SearchUtil.getLogger().log(Level.INFO, "projectId: " + projectId);
-				}
-				
-				// TODO: use the keyword to search for each project 
-//				String keyword = "AutoLogin";
-				HttpResponse getSearchResultProjectsResp = this.client.execute(new GetSearchResultHttpGet(projectId, keyword));
-				String getSearchResultRespContent = EntityUtils.toString(getSearchResultProjectsResp.getEntity());
-				SearchUtil.getLogger().log(Level.INFO, "here");				
-				
-				// TODO: if found sth., restore the page in the final result list 
-				if (!getSearchResultRespContent.contains("We couldn't find any results matching")) {
-					URI searchURI = new URIBuilder("https://gitlab.com/search")
-										.setParameter("utf8", "✓") // %E2%9C%93
-										.setParameter("project_id", projectId)
-										.setParameter("search", keyword)
-										.build();
-					searchObjs.add(searchURI.toString());
-				}
-				
-			} // end of while 
+					String projectId = matcher_projectId.group(1);
+					SearchUtil.getLogger().log(Level.INFO, "get projectId: " + projectId);
+
+					/** search the keyword for each project **/ 
+					HttpResponse getSearchResultProjectsResp = this.client.execute(new GetSearchResultHttpGet(projectId, keyword));
+					String getSearchResultRespContent = EntityUtils.toString(getSearchResultProjectsResp.getEntity());
+					
+					/** if found sth., restore the page in the final result list **/
+					if (!getSearchResultRespContent.contains("We couldn't find any results matching")) {
+						SearchUtil.getLogger().log(Level.INFO, "find result for projectId: " + projectId);
+						URI searchURI = new URIBuilder("https://gitlab.com/search")
+								.setParameter("utf8", "✓") // %E2%9C%93
+								.setParameter("project_id", projectId)
+								.setParameter("search", keyword)
+								.build();
+						searchObjs.add(searchURI.toString());
+					}else {
+						SearchUtil.getLogger().log(Level.INFO, "no result for projectId: " + projectId);
+					}
+				} // end of if (matcher_projectId.find())
+			} // end of while (matcher_project.find())
 			
-			/** create a simple html with a list of projects that have some results from the keyword **/
-			for (String searchObj : searchObjs) {
-				result.append("<li><a href=\"" + searchObj + "\">" + searchObj + "</a></li>");
-			}
-			result.append("</ul>");
-			result.append("</body></html>");
+			
 		} catch (IOException | URISyntaxException e) {
 			SearchUtil.getLogger().log(Level.INFO, e);
 		}
 		
-		
+		/** create a simple html with a list of projects that have some results from the keyword **/
 		String render = html(
 			    head(
 			        title("HTML from API Gateway/Lambda")
